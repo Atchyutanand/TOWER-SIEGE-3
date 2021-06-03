82a0867c39453c9e5ff902d7088be2acd70d2c97
Atchyutanand <78865290+Atchyutanand@users.noreply.github.com>
2021-06-03 20:03:57 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,23 @@
+class Block{
+    constructor(x, y, width, height, angle) {
+        var options = {
+          "restitution" : 0.7,
+          "friction" : 0.6,
+          "isStatic" : false,
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        if(this.body.speed<3){
+          push();
+          translate(this.body.position.x, this.body.position.y);
+          rectMode(CENTER);
+          fill(995,95,19);
+          rect(0, 0, this.width, this.height);
+          pop();
+        }
+      }
+}
